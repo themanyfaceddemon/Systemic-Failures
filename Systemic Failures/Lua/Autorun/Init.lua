942c3db4d63d9931aad74cdc9aa4cbf52bc20dd9
themanyfaceddemon <neko-d-05@mail.ru>
2025-12-23 11:04:13 +0300

    Add Effects

---
+++ Systemic Failures/Lua/Autorun/Init.lua
@@ -0,0 +1,5 @@
+local path = table.pack(...)[1]
+
+dofile(path .. "/Lua/Hooks/ItemHooks.lua")
+
+print("SF loaded")
---
+++ Systemic Failures/Lua/Hooks/ItemHooks.lua
@@ -0,0 +1,44 @@
+local SFRandomDispatch = {}
+SFRandomDispatch["spawnitem"] = function(ctx, item, action)
+    if Game.IsMultiplayer and CLIENT then return end
+    local to_inv = action.GetAttributeString("sameinventory") == "true"
+
+    Entity.Spawner.AddItemToSpawnQueue(
+        ItemPrefab.GetItemPrefab(action.GetAttributeString("identifier")),
+        item.worldPosition,
+        nil,
+        nil,
+        function(newitem)
+            if not to_inv then return end
+
+            local suc = item.ParentInventory.TryPutItemInAnySlot(newitem)
+            if not suc then
+                if Game.IsMultiplayer and CLIENT then return end
+                Entity.Spawner.AddEntityToRemoveQueue(item)
+            end
+        end
+    )
+end
+
+SFRandomDispatch["fire"] = function(ctx, item, action)
+    local fire = FireSource(item.worldPosition, item.CurrentHull, nil, true)
+    fire.Size = Vector2(tonumber(action.GetAttributeString("size")), fire.Size.Y)
+end
+
+Hook.Add("SF.Random", function(effect, deltaTime, item, targets, worldPosition, element)
+    local chance = tonumber(element.GetAttributeString("chance", "0"))
+    if math.random() >= chance then return end
+
+    local actionType = element.GetAttributeString("type")
+    local handler = SFRandomDispatch[actionType]
+    if handler then
+        handler({
+            effect = effect,
+            targets = targets,
+            worldPosition = worldPosition,
+            deltaTime = deltaTime
+        }, item, element)
+    end
+end)
+
+print("SF ItemHooks init done")
